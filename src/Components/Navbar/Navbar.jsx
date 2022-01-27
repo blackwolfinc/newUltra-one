a120ac5fac47e0ef4d9cabfd5d946dc2a2ddf589
Black Wolf Team <47728533+blackwolfinc@users.noreply.github.com>
2022-01-28 01:29:13 +0700

    add header Effec

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Grid, Link } from "@material-ui/core";
 import logo from "../../Assets/img/LogoUltra.png";
-import DehazeIcon from '@material-ui/icons/Dehaze';
+import DehazeIcon from "@material-ui/icons/Dehaze";
 export const Navbar = () => {
+  const [OpenModal, setOpenModal] = useState(true);
   return (
     <div>
       <Container maxWidth={"lg"}>
-        <Grid data-aos="fade-up" data-aos-duration="1000" container className="ContainerHeaderLogo">
-          <Grid item sm={10} md={5} className="HeaderLogo">
-            <img src={logo} alt="" />
+        <Grid
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          container
+          className="ContainerHeaderLogo"
+        >
+          <Grid item sm={10} md={5} className="HeaderLogo" >
+            <img  src={logo} alt="" />
           </Grid>
           <Grid item sm={12} md={5} className="HeaderBody">
             <Grid container spacing={1}>
@@ -30,11 +36,31 @@ export const Navbar = () => {
           <Grid item sm={12} md={2} className="HeaderButton">
             <button>CONTACT US</button>
           </Grid>
-          <Grid item sm={2} md={5} className="HeaderButton2">
-            <button><DehazeIcon/></button>
+          <Grid item  onClick={()=>{
+            setOpenModal(!OpenModal)
+            } } sm={2} md={5} className="HeaderButton2">
+            <button>
+              <DehazeIcon />
+            </button>
           </Grid>
         </Grid>
       </Container>
+      {OpenModal ?  <Grid container className="showHeaderText">
+        <Grid className="nav-itemHeader" item sm={12} md={2}>
+          <a href="">Products</a>
+        </Grid>
+        <Grid className=" width nav-itemHeader" item sm={12} md={3}>
+          <a href="">Our network</a>
+        </Grid>
+        <Grid className=" nav-itemHeader" item sm={12} md={3}>
+          <a href="">About us</a>
+        </Grid>
+
+        <Grid className="Number nav-itemHeader" item sm={12} md={4}>
+          <a href="">8(555) 560-9998</a>
+        </Grid>
+      </Grid> : "" }
+     
     </div>
   );
 };
