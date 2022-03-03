b0c30d0a2c2e5afe295410220b01303cd1a2c17c
Black Wolf Team <47728533+blackwolfinc@users.noreply.github.com>
2022-03-03 17:32:16 +0700

    this

--- src/Pages/Page1/page1Section7/P1S7.jsx
+++ src/Pages/Page1/page1Section7/P1S7.jsx
@@ -11,129 +11,110 @@ import iconSocial4 from "../../../Assets/Icon/iconSocial/Social Icons4.png";
 import LocationBackground from "../../../Assets/img/Locations.png";
 export const P1S7 = () => {
   return (
-    <Container maxWidth="xl" className="ContainerLocationWarp">
-      <Grid container className="OurLocationWarp">
-        <Grid
-          item
-          md={7}
-          sm={12}
-          className="containerKananLocation"
-          data-aos="fade-up"
-          data-aos-duration="1800"
-        >
-          {/* <img src={LocationBackground} alt="" className="imgLocationCostum" /> */}
-          <h1>Our Locations</h1>
-          <Grid container className="KananBody">
-            <Grid item xs={12} sm={12} md={6}>
-              <Grid container direction="column">
-                <Grid item className="LocationCard" xs={12} sm={12} md={12}>
-                  <div className="CardInside">
-                    <img src={iconLocation1} alt="" />
-                    <h2>New York</h2>
-                    <h3>
-                    New York City
-                    </h3>
-                    <a href="">To the map</a>
-                  </div>
-                </Grid>
-                <Grid item className="LocationCard" sm={12} md={12}>
-                  <div className="CardInside">
-                    <img src={iconLocation3} alt="" />
-                    <h2>Miami</h2>
-                    <h3>
-                    Florida
-                    </h3>
-                    <a href="">To the map</a>
+    <div className="Container-Location">
+      {" "}
+      <Container maxWidth="xl" className="ContainerLocationWarp">
+        <Grid container className="OurLocationWarp">
+          <Grid md={12} className="ourLocationText">
+            <h1>Our Locations</h1>
+            <h4>
+              Experience uncompromising performance with our bare-metal servers.
+              Our servers by <br /> default are packaged with our Unlimited
+              priority traffic plan
+            </h4>
+          </Grid>
 
-                  </div>
-                </Grid>
-              </Grid>
-            </Grid>
-            <Grid item  xs={12} sm={12} md={6} className="downBottom">
-              <Grid container direction="column">
-                <Grid item className="LocationCard" sm={12} md={12}>
-                  <div className="CardInside">
-                    <img src={iconLocation2} alt="" />
-                    <h2>Ashburn</h2>
-                    <h3>
-                    Virginia
-                    </h3>
-                    <a href="">To the map</a>
-                  </div>
-                </Grid>
-                <Grid item className="LocationCard" sm={12} md={12}>
-                  <div className="CardInside">
-                    <img src={iconLocation4} alt="" />
-                    <h2>London</h2>
-                    <h3>
-                    United Kingdom
-                    </h3>
-                    <a href="">To the map</a>
-                  </div>
-                </Grid>
-              </Grid>
+          <Grid
+            item
+            md={7}
+            sm={12}
+            className="containerKananLocation"
+            data-aos="fade-up"
+            data-aos-duration="1800"
+          >
+            {/* <img src={LocationBackground} alt="" className="imgLocationCostum" /> */}
+
+            <Grid container className="KananBody">
+            <div className="Line-1"></div>
+            <div className="Line-2"></div>
+            <div className="Line-3"></div>
+            <div className="Line-4"></div>
+              <div className="CardInside NewYork">
+                <img src={iconLocation1} alt="" />
+                <div>  <h2>New York</h2>
+                <p>New York City</p></div>
+              </div>
+
+              <div className="CardInside Miami">
+                <img src={iconLocation3} alt="" />
+                <h2>Miami</h2>
+                <p>Florida</p>
+              </div>
+
+              <div className="CardInside Ashburn">
+              <h2>Ashburn</h2>
+                <p>Virginia</p>
+                <img src={iconLocation2} alt="" />
+               
+              </div>
+
+              <div className="CardInside London">
+                <img src={iconLocation4} alt="" />
+                <div>       <h2>London</h2>
+                <p>United Kingdom</p></div>
+              </div>
             </Grid>
           </Grid>
-        </Grid>
-        <Grid item sm={12} md={5}>
-          <Grid container className="ContainerkiriLocation">
-            <Grid item md={12}>
-              <div
-                className="IconSocial"
+          <Grid item sm={12} md={5}>
+            <Grid container className="ContainerkiriLocation">
+              {/* <Grid item md={12}>
+            
+            </Grid> */}
+              <Grid
+                item
+                md={12}
+                sm={12}
+                className="ContactWarper WapSec"
                 data-aos="fade-up"
-                data-aos-duration="800"
+                data-aos-duration="1500"
               >
-                <h4>Follow Us</h4>
-                <img src={iconSocial} alt="" />
-                <img src={iconSocial2} alt="" />
-                <img src={iconSocial3} alt="" />
-                <img src={iconSocial4} alt="" />
-              </div>
-            </Grid>
-            <Grid
-              item
-              md={12}
-              sm={12}
-              className="ContactWarper WapSec"
-              data-aos="fade-up"
-              data-aos-duration="1500"
-            >
-              <h1>Contacts</h1>
-              <div className="conatact">
-                <h5>Support Portal</h5>
-                <p> support.ultratelecom.net </p>
-              </div>
-              <div className="conatact">
-                <h5>Phone Support</h5>
-                <p>+1 (307) 200-8584 </p>
-              </div>
-              <div className="conatact">
-                <h5>Email Support</h5>
-                <p> support@ultratelecom.net </p>
-              </div>
-              <div className="conatact">
-                <h5>Address</h5>
-                <p>A 30N Gould ST, Sheridan, WY 82801, US</p>
-              </div>
-            </Grid>
-            <Grid
-              item
-              md={12}
-              sm={12}
-              className="ContactWarper WapSec"
-              data-aos="fade-up"
-              data-aos-duration="1800"
-            >
-              <h1>Get in Touch</h1>
-              <h3>Sign Up to our newletter</h3>
-              <div className="emailSubmit">
-                <input type="text" placeholder="Your Email" />
-                <button className="ButtonSignUp"> Sign Up</button>
-              </div>
+                <h1>Contacts</h1>
+                <div className="conatact">
+                  <h5>Support Portal</h5>
+                  <p> support.ultratelecom.net </p>
+                </div>
+                <div className="conatact">
+                  <h5>Phone Support</h5>
+                  <p>+1 (307) 200-8584 </p>
+                </div>
+                <div className="conatact">
+                  <h5>Email Support</h5>
+                  <p> support@ultra.one</p>
+                </div>
+                <div className="conatact">
+                  <h5>Address</h5>
+                  <p>A 30N Gould ST, Sheridan, WY 82801, US</p>
+                </div>
+              </Grid>
+              <Grid
+                item
+                md={12}
+                sm={12}
+                className="ContactWarper WapSec getTouch"
+                data-aos="fade-up"
+                data-aos-duration="1800"
+              >
+                <h1>Get in Touch</h1>
+                <h3>Sign Up to our newletter</h3>
+                <div className="emailSubmit">
+                  <input type="text" placeholder="Your Email" />
+                  <button className="ButtonSignUp"> Sign Up</button>
+                </div>
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </div>
   );
 };
--- src/Pages/Page1/page1Section8/P1S8.jsx
+++ src/Pages/Page1/page1Section8/P1S8.jsx
@@ -1,8 +1,11 @@
 import { Container, Grid } from '@material-ui/core';
 import React from 'react';
-
+import iconSocial from "../../../Assets/Icon/iconSocial/Social Icons.png";
+import iconSocial2 from "../../../Assets/Icon/iconSocial/Social Icons2.png";
+import iconSocial3 from "../../../Assets/Icon/iconSocial/Social Icons3.png";
+import iconSocial4 from "../../../Assets/Icon/iconSocial/Social Icons4.png";
 export const P1S8 = () => {
-  return   <Container maxWidth={"xl"} className="footerContainer">
+  return  <div className='footer-Container'>   <Container maxWidth={"xl"} className="footerContainer">
   <Grid container>
     <Grid item  xs={12} sm={12} md={2}>
       <a href="">© 2022 Ultra.One</a>{" "}
@@ -21,6 +24,13 @@ export const P1S8 = () => {
         <Grid item className="FooterA" xs={12} sm={12} md={1}>
           <a href="">Contacts</a>
         </Grid>
+        <Grid  item className="FooterA IconSocial"  xs={12} sm={12} md={2}>  
+             
+                <img src={iconSocial} alt="" />
+                <img src={iconSocial2} alt="" />
+                <img src={iconSocial3} alt="" />
+                <img src={iconSocial4} alt="" />
+            </Grid>
       </Grid>
     </Grid>
     <Grid item  xs={12} sm={12} md={2}>
@@ -28,4 +38,6 @@ export const P1S8 = () => {
     </Grid>
   </Grid>
 </Container>;
+</div>
+
 };
